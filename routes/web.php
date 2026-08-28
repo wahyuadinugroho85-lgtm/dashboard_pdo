dc63b8198e719c0f20f370ee96889fd285cf2ae2
wahyuadinugroho85-lgtm <wahyuadinugroho85@gmail.com>
2026-08-28 19:01:09 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Report\MonthlyDashboardController;
 use App\Http\Controllers\UserController; 
 
-// Ini adalah baris ajaib yang baru saja dibuat oleh Laravel untuk mengatur Login/Register/Logout
+// Rute Bawaan Auth Laravel
 Auth::routes();
 
 // Rute awal (otomatis dilempar ke laporan manajemen)
@@ -13,25 +13,27 @@ Route::get('/', function () {
     return redirect('/laporan-manajemen'); 
 });
 
-// KUMPULAN RUTE YANG DIKUNCI (Wajib Login)
+// KUMPULAN RUTE UMUM (Semua User Login Bisa Akses)
 Route::middleware(['auth'])->group(function () {
-    
-    // Route Dashboard
     Route::get('/laporan-manajemen', [MonthlyDashboardController::class, 'index']);
-
-    // Route Input, Import, & Export Data
     Route::get('/input-data', [MonthlyDashboardController::class, 'create']);
     Route::post('/input-data', [MonthlyDashboardController::class, 'store']);
     Route::post('/import-data', [MonthlyDashboardController::class, 'importExcel']);
-    Route::get('/export-data', [MonthlyDashboardController::class, 'exportData'])->name('export.data'); // <-- ROUTE EXPORT BARU
+    Route::get('/export-data', [MonthlyDashboardController::class, 'exportData'])->name('export.data');
     Route::get('/download-template', [MonthlyDashboardController::class, 'downloadTemplate']);
-    
-    // Route Kelola User
+});
+
+// KUMPULAN RUTE TERKUNCI (HANYA ADMIN)
+Route::middleware(['auth', function ($request, $next) {
+    if (Auth::user()->role !== 'admin') {
+        abort(403, 'Akses Ditolak! Hanya Admin yang dapat mengelola user.');
+    }
+    return $next($request);
+}])->group(function () {
     Route::get('/kelola-user', [UserController::class, 'index'])->name('kelola.user');
     Route::post('/kelola-user', [UserController::class, 'store'])->name('kelola.user.store');
     Route::put('/kelola-user/{id}', [UserController::class, 'update'])->name('kelola.user.update');
     Route::delete('/kelola-user/{id}', [UserController::class, 'destroy'])->name('kelola.user.destroy');
-    
 });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
